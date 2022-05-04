f95ffe582ddee9bc9a8ab75bb0515a29d23f18c0
apynx <safriladi@gmail.com>
2022-05-04 17:28:18 +0700

    update disqus

--- assets/js/ockham.js
+++ assets/js/ockham.js
@@ -13,3 +13,30 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+
+var disqus_loaded = false;
+var disqus_enabled = document.getElementById("load-comments");
+$("#load-comments").on("click", load_disqus);
+
+window.onscroll = function (e) {
+  scrollFunction();
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    if (disqus_enabled && disqus_loaded == false) {
+      load_disqus();
+    }
+  }
+};
+
+function load_disqus() {
+  if (disqus_loaded || !disqus_enabled) return;
+  var disqus_shortname = "{{site.disqus}}";
+  $.ajax({
+    type: "GET",
+    url: "https://" + disqus_shortname + ".disqus.com/embed.js",
+    dataType: "script",
+    cache: true,
+  });
+  disqus_loaded = true;
+  $(".show-comments").fadeOut();
+}
